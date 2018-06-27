ced2d2c3389433b3be749374e7af2e4f0ba9beff
Ryan Rathsam <ryanrath@buffalo.edu>
2018-06-27 09:30:10 -0400

    Removing tests for unused code paths

--- open_xdmod/modules/xdmod/component_tests/lib/Roles/CenterStaffRoleTest.php
+++ open_xdmod/modules/xdmod/component_tests/lib/Roles/CenterStaffRoleTest.php
@@ -13,43 +13,6 @@ use CCR\Json;
  **/
 class CenterStaffRoleTest extends BaseTest
 {
-    /**
-     * @expectedException Exception
-     * @expectedExceptionMessage No user ID has been assigned to this role.  You must call configure() before calling getCorrespondingUserID()
-     */
-    public function testGetIdentifierAbsoluteNoUser()
-    {
-        $cs = new CenterStaffRole();
-        $cs->getIdentifier(true);
-    }
-
-    /**
-     * @dataProvider provideGetIdentifierAbsolute
-     * @param string $aclName Name of the ACL we are checking
-     * @param string $userName Name of the user we are checking
-     * @param int $centerId center identifier (organization_id)
-     */
-
-    public function testGetIdentifierAbsolute($aclName, $userName, $centerId)
-    {
-        // It is expected that when `getIdentifier` is called w/ an
-        // `absolute_identifier` of true. That result will contain the center
-        // that the user is associated with in the form: <acl_name>;<center>
-        $expected = sprintf('%s;%d', self::CENTER_STAFF_ACL_NAME, $centerId);
-        $user = XDUser::getUserByUserName(self::CENTER_STAFF_USER_NAME);
-        $cs = new CenterStaffRole();
-        $cs->configure($user);
-        $actual = $cs->getIdentifier(true);
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function provideGetIdentifierAbsolute()
-    {
-        return Json::loadFile(
-            $this->getTestFiles()->getFile('roles', 'get_identifer_absolute')
-        );
-    }
-
     public function testGetIdentifier()
     {
         $expected = self::CENTER_STAFF_ACL_NAME;
