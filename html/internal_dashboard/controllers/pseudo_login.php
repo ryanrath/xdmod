3d999f88389af69dddd698d34cda4842dc45e3df
Ryan Rathsam <ryanrath@buffalo.edu>
2018-11-01 13:31:37 -0400

    Reverting unnecessary change
    After running all tests I wasn't able to reproduce the error.

--- html/internal_dashboard/controllers/pseudo_login.php
+++ html/internal_dashboard/controllers/pseudo_login.php
@@ -11,7 +11,7 @@
    
    function getAbsoluteURL() {
    
-      $protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
+      $protocol = ($_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
       
       return $protocol.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
    
