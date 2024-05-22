c09bb96be04ceff42c9207920d3db2e87a7acaa9
Ryan Rathsam <rrathsam@gmail.com>
2024-05-22 11:18:31 -0400

    Adding documentation to the Populate days script
    Adding a doc block in an effort to describe the technique that is being
    used in these SQL statements to populate the days|months|quarters|years
    tables.

--- configuration/etl/etl_sql.d/common/populate_day_table.sql
+++ configuration/etl/etl_sql.d/common/populate_day_table.sql
@@ -1,3 +1,16 @@
+/*
+This is probably the easiest of the four queries to explain and all of them work on the same basic principle.
+This method of generation uses a Recursive CTE ( https://mariadb.com/kb/en/recursive-common-table-expressions-overview/ )
+in this case `days` to dynamically generate records for the `modw.days` table until the `WHERE` clause is no longer
+true, in this case when `date` >= "2038-01-18".
+
+It does this by using a standard recursive cte pattern whereby two SELECT statements are `UNION ALL`'d. The base query
+executes first, taking whatever it needs to compute the result R0. The second recursive query is executed taking R0 as
+input — that is R references R0 in the recursive query when first executed. The recursive query produces the result R1,
+and that is what R will reference at the next invocation, and so on until a recursive query returns an empty result. At
+that point, all intermediate results are combined together.
+*/
+
 INSERT INTO
     modw.days
 WITH RECURSIVE days(date, year, id, day, day_start, day_end, hours, seconds, day_start_ts, day_end_ts, day_middle_ts)
