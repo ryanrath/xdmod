d1b7db48e6d70a74b6f0e7261fa13bc7b9bec73e
Ryan Rathsam <ryanrath@buffalo.edu>
2026-04-22 14:32:59 -0400

    removing unneeded code

--- tests/ci/bootstrap.sh
+++ tests/ci/bootstrap.sh
@@ -195,14 +195,3 @@ then
     # Restart so that the above changes take effect.
     ~/bin/services restart
 fi
-
-# ensure that we use the correct referer for SSO in the CI Build until we get Keycloak integrated.
-line_no=$(grep -n 'show_local_login = "off"' /etc/xdmod/portal_settings.ini | cut -d':' -f1)
-
-# Add the auth_referer property to portal_settings.ini
-sed -i "${line_no}a\
-auth_referer=https://xdmod:7000
-" /etc/xdmod/portal_settings.ini
-
-# Make sure that the cache is reset so that `auth_referer` shows up in Symfony at runtime.
-console cache:clear
