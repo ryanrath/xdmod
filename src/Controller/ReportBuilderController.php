8bd66541677d915cf9a73af9b2de14e81de60e26
Ryan Rathsam <ryanrath@buffalo.edu>
2026-05-18 17:03:03 -0400

    Fixing reporting_image_renderer.php once and for all

--- src/Controller/ReportBuilderController.php
+++ src/Controller/ReportBuilderController.php
@@ -508,7 +508,7 @@ class ReportBuilderController extends BaseController
      * @throws Exception
      */
     #[Route('/reports/builder/image', methods: ['GET'])]
-    #[Route('/report_image_renderer.php', name: 'report_image_renderer_legacy', methods: ['GET'])]
+    #[Route('/controllers/report_image_renderer.php', name: 'report_image_renderer_legacy',requirements: ['prefix' => '.*'], methods: ['GET'])]
     public function generateReportImage(Request $request): Response
     {
         $this->logger->debug('Generating report image');
