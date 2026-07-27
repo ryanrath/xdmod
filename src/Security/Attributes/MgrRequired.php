6e8ac789827f0f32085e2ec9b62d0bbfbe00a843
Ryan Rathsam <ryanrath@buffalo.edu>
2026-07-27 10:38:24 -0500

    Implementing MgrRequired feature
    These two files work together to utilize Symfony's built in systems (
    Routes, Events and Event Listeners ) to implement a feature that could
    be described as "For a given route, require that the user have the mgr
    acl to be authorized".
    
    To accomplish this is a two part process. First, add the `MgrRequired`
    Attribute to a route that should only be accessed by users who are
    considered to be "Managers" ( i.e. who have access to the Internal
    Dashboard ). Then, when this route is requested, during the normal
    processing of this route a "ControllerArgument" event will be generated
    and handled by the `MgrRequiredAttributeListener` class.

---
+++ src/Security/Attributes/MgrRequired.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace CCR\Security\Attributes;
+
+use CCR\Security\Listeners\MgrRequiredAttributeListener;
+use Symfony\Component\ExpressionLanguage\Expression;
+
+/**
+ * This attribute, when used to decorate a Symfony Controller class or Route function, restricts which users are
+ * authorized to request the Controller or Route to those with the Manager Acl ( those who have access to the Internal
+ * Dashboard. ).
+ *
+ * By pairing this Attribute with an accompanying AttributeListener ( @see MgrRequiredAttributeListener ) class attached
+ * to the Kernel Controller Event, we are able to provide the "Restrict this route to users w/ the specified ACL".
+ *
+ */
+#[\Attribute(\Attribute::IS_REPEATABLE | \Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_FUNCTION)]
+class MgrRequired
+{
+    /**
+     * This is the expression that ensures that a user that requests an endpoint decorated with this attribute *must*
+     * have the "mgr" acl, else an AccessDeniedHttpException will be thrown ( @see MgrRequiredAttributeListener ).
+     *
+     * @var string
+     */
+    const EXPRESSION = '"mgr" in role_names';
+
+    public function __construct(
+
+        /**
+         * Sets the first argument that will be passed to isGranted().
+         */
+        public string|Expression            $attribute = new Expression(self::EXPRESSION),
+
+        /**
+         * Sets the second argument passed to isGranted().
+         *
+         * @var array<string|Expression>|string|Expression|null
+         */
+        public array|string|Expression|null $subject = null,
+
+        /**
+         * The message of the exception - has a nice default if not set.
+         */
+        public ?string                      $message = null,
+
+        /**
+         * If set, will throw HttpKernel's HttpException with the given $statusCode.
+         * If null, Security\Core's AccessDeniedException will be used.
+         */
+        public ?int                         $statusCode = null,
+
+        /**
+         * If set, will add the exception code to thrown exception.
+         */
+        public ?int                         $exceptionCode = null,
+    )
+    {
+    }
+}
---
+++ src/Security/Listeners/MgrRequiredAttributeListener.php
@@ -0,0 +1,112 @@
+<?php declare(strict_types=1);
+
+namespace CCR\Security\Listeners;
+
+
+use CCR\Security\Attributes\MgrRequired;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\ExpressionLanguage\Expression;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\RuntimeException;
+
+/**
+ * This class' purpose is to listen for the KernelControllerArguments event and if the event has the `MgrRequired`
+ * Attribute, proceed to check the specified subject ( the user requesting the controller ) conforms to the requirement
+ * provided by `MgrRequired`. If the user does, then they are authorized and processing continues, if they do not, then an
+ * `AccessDeniedHttpException` is thrown.
+ */
+class MgrRequiredAttributeListener implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly AuthorizationCheckerInterface $authChecker,
+        private ?ExpressionLanguage $expressionLanguage = null,
+
+    ) {
+    }
+
+    /**
+     * @param ControllerArgumentsEvent $event
+     * @return void
+     */
+    public function onKernelControllerArguments(ControllerArgumentsEvent $event): void
+    {
+        /** @var MgrRequired[] $attributes */
+        if (!\is_array($attributes = $event->getAttributes()[MgrRequired::class] ?? null)) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        $arguments = $event->getNamedArguments();
+        foreach ($attributes as $attribute) {
+            $subject = null;
+
+            if ($subjectRef = $attribute->subject) {
+                if (\is_array($subjectRef)) {
+                    foreach ($subjectRef as $refKey => $ref) {
+                        $subject[\is_string($refKey) ? $refKey : (string) $ref] = $this->getIsGrantedSubject($ref, $request, $arguments);
+                    }
+                } else {
+                    $subject = $this->getIsGrantedSubject($subjectRef, $request, $arguments);
+                }
+            }
+
+            if (!$this->authChecker->isGranted($attribute->attribute, $subject)) {
+                $message = $attribute->message ?: \sprintf('Access Denied by #[MgrRequired(%s)] on controller', $this->getIsGrantedString($attribute));
+
+                if ($statusCode = $attribute->statusCode) {
+                    throw new HttpException($statusCode, $message, code: $attribute->exceptionCode ?? 0);
+                }
+                throw new AccessDeniedHttpException($message, code: $attribute->exceptionCode ?? 403);
+            }
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [KernelEvents::CONTROLLER_ARGUMENTS => ['onKernelControllerArguments', 30]];
+    }
+
+    private function getIsGrantedSubject(string|Expression $subjectRef, Request $request, array $arguments): mixed
+    {
+        if ($subjectRef instanceof Expression) {
+            $this->expressionLanguage ??= new ExpressionLanguage();
+
+            return $this->expressionLanguage->evaluate($subjectRef, [
+                'request' => $request,
+                'args' => $arguments,
+            ]);
+        }
+
+        if (!\array_key_exists($subjectRef, $arguments)) {
+            throw new RuntimeException(\sprintf('Could not find the subject "%s" for the #[MgrRequired] attribute. Try adding a "$%s" argument to your controller method.', $subjectRef, $subjectRef));
+        }
+
+        return $arguments[$subjectRef];
+    }
+
+    private function getIsGrantedString(MgrRequired $isGranted): string
+    {
+        $processValue = fn ($value) => \sprintf($value instanceof Expression ? 'new Expression("%s")' : '"%s"', $value);
+
+        $argsString = $processValue($isGranted->attribute);
+
+        if (null !== $subject = $isGranted->subject) {
+            $subject = !\is_array($subject) ? $processValue($subject) : array_map(function ($key, $value) use ($processValue) {
+                $value = $processValue($value);
+
+                return \is_string($key) ? \sprintf('"%s" => %s', $key, $value) : $value;
+            }, array_keys($subject), $subject);
+
+            $argsString .= ', '.(!\is_array($subject) ? $subject : '['.implode(', ', $subject).']');
+        }
+
+        return $argsString;
+    }
+}
+
